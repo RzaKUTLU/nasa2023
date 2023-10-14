ea5f5956deee15dd7aceedaf97ec02af42f07abf
RzaKUTLU <114239738+RzaKUTLU@users.noreply.github.com>
2023-10-14 14:12:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 import numpy as np
 import streamlit as st
 
-def drone():
-    page_by_img = """
-             <style>
-             [data-testid="stAppViewContainer"]{
-             background: url("https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg");
-             background-size: cover;top: 0px}
 
-             [data-testid="stHeader"]{
-             background-color: rgba(0,0,0,0);
-              }
-
-             [data-testid="stToolbar"]{
-             right: 2rem;}
-             </style>
-              """
-    st.markdown(page_by_img, unsafe_allow_html=True)
-    st.text("")
-    st.text("")
 
     st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Fire Fighting with Drone WEBSITE</div>',
                 unsafe_allow_html=True)
