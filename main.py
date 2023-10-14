8642cb76b6e76b14e355b829793fdead7389dc1e
RzaKUTLU <114239738+RzaKUTLU@users.noreply.github.com>
2023-10-14 14:43:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import streamlit as st
 
 def drone():
 
-
-    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Fire Fighting with Drone WEBSITE</div>',
-                unsafe_allow_html=True)
-
     # YouTube video link
     video_link = "https://youtu.be/bRrNiXUAR7I"
     st.video(video_link)
