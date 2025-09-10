49ef78bd4f1bc3d98e6435c7c25852d8af106e58
Rıza <114239738+RzaKUTLU@users.noreply.github.com>
2025-09-10 18:36:01 +0300

    fix: close all open HTML tags in headings to avoid DOM errors

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def ana_sayfa():
     st.markdown(f'<div style="color: white; font-weight: bold;">{text}</div>', unsafe_allow_html=True)
     st.write("")
 
-    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Marmaris', unsafe_allow_html=True)
+    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Marmaris</div>', unsafe_allow_html=True)
 
     image_comparison(
         img1="https://i01.sozcucdn.com/wp-content/uploads/2021/08/02/marmaris-yangin2-once-sonra.jpg",
@@ -49,7 +49,7 @@ def ana_sayfa():
 
     )
 
-    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Kalkan', unsafe_allow_html=True)
+    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Kalkan</div>', unsafe_allow_html=True)
     image_comparison(
         img1="http://www.detaykibris.com/d/gallery/514_2.jpg",
         img2="http://www.detaykibris.com/d/gallery/514_3.jpg",
@@ -57,7 +57,7 @@ def ana_sayfa():
         label2="Before",
     )
 
-    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Santa Rosa, Kaliforniya',
+    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Santa Rosa, Kaliforniya</div>',
                 unsafe_allow_html=True)
     image_comparison(
         img1="https://static01.nyt.com/newsgraphics/2017/10/09/cali-fires/6aac41a1a254730e98581e3614f8a4aaebd48ee1/coffey-park-after.jpg",
@@ -65,7 +65,7 @@ def ana_sayfa():
         label1="After",
         label2="Before",
     )
-    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Fountaingrove Round Barn',
+    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Fountaingrove Round Barn</div>',
                 unsafe_allow_html=True)
     image_comparison(
         img1="https://static01.nyt.com/newsgraphics/2017/10/09/cali-fires/6aac41a1a254730e98581e3614f8a4aaebd48ee1/barn-before.jpg",
@@ -73,7 +73,7 @@ def ana_sayfa():
         label1="After",
         label2="Before",
     )
-    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Santa Rosa, Fountaingrove bölgesi',
+    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Santa Rosa, Fountaingrove bölgesi</div>',
                 unsafe_allow_html=True)
     image_comparison(
         img1="https://static01.nyt.com/newsgraphics/2017/10/09/cali-fires/6aac41a1a254730e98581e3614f8a4aaebd48ee1/hilton-after.jpg",
@@ -81,7 +81,7 @@ def ana_sayfa():
         label1="After",
         label2="Before",
     )
-    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Avustralya/Flinders Chase Ulusal Parkı',
+    st.markdown('<div style="color: white;font-size: 24px;font-weight: bold;">Avustralya/Flinders Chase Ulusal Parkı</div>',
                 unsafe_allow_html=True)
     image_comparison(
         img1="https://ichef.bbci.co.uk/news/800/cpsprodpb/9488/production/_110542083_058989880.jpg",
@@ -99,15 +99,15 @@ def hakkimizda():
     st.image(image, caption="PNG Resmi", use_column_width=True)
     page_by_img = """
                <style>
-               [data-testid="stAppViewContainer"]{
-               background: url("https://shiftdelete.net/wp-content/uploads/2021/07/atesi-ates-ile-sonduren-drone-orman-yanginlarini-cozumu-olabilir-3.jpg");
+               [data-testid=\"stAppViewContainer\"]{
+               background: url(\"https://shiftdelete.net/wp-content/uploads/2021/07/atesi-ates-ile-sonduren-drone-orman-yanginlarini-cozumu-olabilir-3.jpg\");
                background-size: cover;top: 0px}
 
-               [data-testid="stHeader"]{
+               [data-testid=\"stHeader\"]{
                background-color: rgba(0,0,0,0);
                 }
 
-               [data-testid="stToolbar"]{
+               [data-testid=\"stToolbar\"]{
                right: 2rem;}
                </style>
                 """
@@ -119,15 +119,15 @@ def hakkimizda():
 def drone():
     page_by_img = """
              <style>
-             [data-testid="stAppViewContainer"]{
-             background: url("https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg");
+             [data-testid=\"stAppViewContainer\"]{
+             background: url(\"https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg\");
              background-size: cover;top: 0px}
 
-             [data-testid="stHeader"]{
+             [data-testid=\"stHeader\"]{
              background-color: rgba(0,0,0,0);
               }
 
-             [data-testid="stToolbar"]{
+             [data-testid=\"stToolbar\"]{
              right: 2rem;}
              </style>
               """
@@ -135,52 +135,44 @@ def drone():
     st.text("")
     st.text("")
 
-    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">front cover  </div>',
+    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">front cover</div>',
                 unsafe_allow_html=True)
 
     # YouTube video linki
     video_link = "https://www.youtube.com/watch?v=wCNlORnXpe8"
     st.video(video_link)
 
-    ###2
-
-    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Back cover  </div>',
+    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Back cover</div>',
                 unsafe_allow_html=True)
 
-    # YouTube video linki
     video_link = "https://www.youtube.com/watch?v=KPRjJ67nd2A&ab_channel=BurakhanAlimÇay"
     st.video(video_link)
 
-    ### 3
-
     st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Drone</div>', unsafe_allow_html=True)
 
-    # YouTube video linki
     video_link = "https://www.youtube.com/watch?v=0SGDBw_JWAo&ab_channel=BurakhanAlim%C3%87ay"
     st.video(video_link)
 
-    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Watchtower camera </div>',
+    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Watchtower camera</div>',
                 unsafe_allow_html=True)
 
-    # YouTube video linki
     video_link = "https://www.youtube.com/watch?v=Hawl2DU7MB0&ab_channel=BurakhanAlim%C3%87ay"
     st.video(video_link)
 
-    # Yazıyı beyaz renkte, arka planı kaldırmak ve yazıyı kalın yapmak için stil özellikleri kullanarak HTML kullanım
 
 
 def yangin_nedir():
     page_by_img = """
              <style>
-             [data-testid="stAppViewContainer"]{
-             background: url("https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg");
+             [data-testid=\"stAppViewContainer\"]{
+             background: url(\"https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg\");
              background-size: cover;top: 0px}
 
-             [data-testid="stHeader"]{
+             [data-testid=\"stHeader\"]{
              background-color: rgba(0,0,0,0);
               }
 
-             [data-testid="stToolbar"]{
+             [data-testid=\"stToolbar\"]{
              right: 2rem;}
              </style>
               """
@@ -208,22 +200,21 @@ def yangin_nedir():
              "<br><br><br>")
     st.write("")
     st.markdown(f'<div style="color: white;font-size: 22px; font-weight: bold;">{textd}</div>', unsafe_allow_html=True)
-    image_url = "ABCDK 2 WEB.jpg"
 
 
 
 def termal_kamera():
     page_by_img = """
              <style>
-             [data-testid="stAppViewContainer"]{
-             background: url("https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg");
+             [data-testid=\"stAppViewContainer\"]{
+             background: url(\"https://i.gazeteoksijen.com/storage/files/images/2022/06/01/yasamak-bir-orman-gibi-kardescesine-GDL0.jpg\");
              background-size: cover;top: 0px}
 
-             [data-testid="stHeader"]{
+             [data-testid=\"stHeader\"]{
              background-color: rgba(0,0,0,0);
               }
 
-             [data-testid="stToolbar"]{
+             [data-testid=\"stToolbar\"]{
              right: 2rem;}
              </style>
               """
@@ -231,8 +222,7 @@ def termal_kamera():
     st.text("")
     st.text("")
 
-    #########################################
-    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Thermal camera working principle </div>',
+    st.markdown('<div style="color: white;font-size: 32px;font-weight: bold;">Thermal camera working principle</div>',
             unsafe_allow_html=True)
 
     text2 = """ helps the user to assess the fire risk of each pixel clicked in a thermal image based on the HSV color space. If the clicked region is within a certain color range, it is indicated that this region may be at risk of fire. This can be used for fire detection or intervention in fire zones."""
@@ -242,8 +232,9 @@ def termal_kamera():
     st.video(youtube_url)
 
 
+
 def yangınla_mücadele():
-    st.markdown('<div style="color: black;font-size: 32px;font-weight: bold;">BİLGİLENDİRME', unsafe_allow_html=True)
+    st.markdown('<div style="color: black;font-size: 32px;font-weight: bold;">BİLGİLENDİRME</div>', unsafe_allow_html=True)
 
     # gömme kodları
     embed_code = """
@@ -292,15 +283,15 @@ def yangınla_mücadele():
 def iletisim():
     page_by_img = """
              <style>
-             [data-testid="stAppViewContainer"]{
-             background: url("https://images.pexels.com/photos/821754/pexels-photo-821754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+             [data-testid=\"stAppViewContainer\"]{
+             background: url(\"https://images.pexels.com/photos/821754/pexels-photo-821754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2\");
              background-size: cover;top: 0px}
 
-             [data-testid="stHeader"]{
+             [data-testid=\"stHeader\"]{
              background-color: rgba(0,0,0,0);
               }
 
-             [data-testid="stToolbar"]{
+             [data-testid=\"stToolbar\"]{
              right: 2rem;}
              </style>
               """
